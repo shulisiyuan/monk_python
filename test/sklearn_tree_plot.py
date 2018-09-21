f9f8a32cf4371155fb5b55d0abbf7b17e84a51e4
shulisiyuan <shulisiyuan@163.com>
2018-09-21 17:06:25 +0800

    公司笔记本

---
+++ test/sklearn_tree_plot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 13 18:53:03 2018
+
+@author: monk
+"""
+
+import numpy as np
+import matplotlib
---
+++ test/test3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 18 11:08:02 2018
+
+@author: monk
+"""
+
+import matplotlib.pyplot as plt
